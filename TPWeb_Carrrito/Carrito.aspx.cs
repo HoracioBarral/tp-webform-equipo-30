52262ab2c3663ff0caa47e29474b3925cad88d26
HoracioBarral <horacio.barral@hotmail.com>
2023-10-12 17:12:19 -0300

    Se agrega funcionalidad al boton de Eliminar en el carrito

--- TPWeb_Carrrito/Carrito.aspx.cs
+++ TPWeb_Carrrito/Carrito.aspx.cs
@@ -11,11 +11,16 @@ namespace TPWeb_Carrrito
 {
     public partial class Carrito : System.Web.UI.Page
     {
+        public List<Articulo> artAgregados { get; set; }
+        public List<Articulo> listadoArticulos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                repeaterCarrito.DataSource = (List<Articulo>)Session["artAgregados"];
+                artAgregados = new List<Articulo>();
+                artAgregados = (List<Articulo>)Session["artAgregados"];
+                Session.Add("listadoArticulos", artAgregados);
+                repeaterCarrito.DataSource = artAgregados;
                 repeaterCarrito.DataBind();
             }
         }
@@ -23,7 +28,12 @@ namespace TPWeb_Carrrito
 
         protected void Eliminar_Click(object sender, EventArgs e)
         {
-
+            int id = int.Parse(((Button)sender).CommandArgument);
+            listadoArticulos = (List<Articulo>)Session["listadoArticulos"];
+            Articulo artEliminado = listadoArticulos.Find(x => x.Id == id);
+            listadoArticulos.Remove(artEliminado);
+            repeaterCarrito.DataSource = listadoArticulos;
+            repeaterCarrito.DataBind();
         }
 
         protected void btnVolver_Click(object sender, EventArgs e)
