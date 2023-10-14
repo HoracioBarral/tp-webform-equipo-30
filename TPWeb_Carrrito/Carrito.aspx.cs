2b5bcb52ba7c54ed3d059a85574afcec9ac39501
HoracioBarral <horacio.barral@hotmail.com>
2023-10-13 21:19:48 -0300

    Se optimiza la funcion de Eliminar producto en el Carrito

--- TPWeb_Carrrito/Carrito.aspx.cs
+++ TPWeb_Carrrito/Carrito.aspx.cs
@@ -11,10 +11,16 @@ namespace TPWeb_Carrrito
 {
     public partial class Carrito : System.Web.UI.Page
     {
-        //public List<Articulo> artAgregados { get; set; }
+        public List<Articulo> artAgregados { get; set; }
         public List<Articulo> listadoArticulos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            artAgregados = (List<Articulo>)Session["artAgregados"];
+            if (!IsPostBack)
+            {
+                repeaterCarrito.DataSource = artAgregados;
+                repeaterCarrito.DataBind();
+            }
             /*if (!IsPostBack)
             {
                 artAgregados = new List<Articulo>();
@@ -23,19 +29,17 @@ namespace TPWeb_Carrrito
                 repeaterCarrito.DataSource = artAgregados;
                 repeaterCarrito.DataBind();
             }*/
-            List<Articulo>artAgregados = (List<Articulo>)Session["artAgregados"];
-            repeaterCarrito.DataSource = artAgregados;
-            repeaterCarrito.DataBind();
         }
 
 
         protected void Eliminar_Click(object sender, EventArgs e)
         {
             int id = int.Parse(((Button)sender).CommandArgument);
-            listadoArticulos = (List<Articulo>)Session["listadoArticulos"];
+            listadoArticulos = (List<Articulo>)Session["artAgregados"];
             Articulo artEliminado = listadoArticulos.Find(x => x.Id == id);
-            listadoArticulos.Remove(artEliminado);
-            repeaterCarrito.DataSource = listadoArticulos;
+            //listadoArticulos.Remove(artEliminado);
+            artAgregados.Remove(artEliminado);
+            repeaterCarrito.DataSource = artAgregados;
             repeaterCarrito.DataBind();
         }
 
